6bbc226a47201fefe3c0a88570a46dbdbecda184
mojfazel <fazeli.mojtaba@gmail.com>
2020-12-02 16:43:09 -0500

    fixes build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  apps: [
-    {
-      name: 'aws-codedeploy',
-      script: 'npx',
-      args: 'serve -s build -l 3000 -n',
-      interpreter: 'none',
-      env: {
-        NODE_ENV: 'development',
-      },
-    },
-  ],
-}
+	apps: [
+		{
+			name: 'rebound-game',
+			script: 'npx',
+			args: 'serve -s build -l 3000 -n',
+			interpreter: 'none',
+			env: {
+				NODE_ENV: 'development'
+			}
+		}
+	]
+};
