0dca3294f8bf96a55825adbf54919eb6cc778fa0
mojfazel <fazeli.mojtaba@gmail.com>
2020-12-03 11:49:42 -0500

    changes the port to 3000

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
 		{
 			name: 'aws-codedeploy',
 			script: 'npx',
-			args: 'http-server -p 8080',
+			args: 'http-server -p 3000',
 			interpreter: 'none',
 			env: {
 				NODE_ENV: 'development'
